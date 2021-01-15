e8890443300aeecafad309ff700e27ef0eed44f4
Nunnalin Bingbing <61963924+snnmbb@users.noreply.github.com>
2021-01-15 15:17:37 +0700

    11_1_1 finished

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int x);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x)
+{
+    if(x>1)
+    {
+        return fibonacci(x-1)+fibonacci(x-2);
+    }
+    else if(x==0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
 }
